e448aa7502b242677eca4db28c8549e0951a048f
sreesree007 <sreeram15122001@gmail.com>
2024-04-08 19:30:14 +0530

    second commit

--- test.java
+++ test.java
@@ -1,6 +1,8 @@
 public Class Hello{
 public static void main(String[] args){
 System.out.println("it is spring...");
-System.out.println("it is winter..");
+System.out.println("it is winter...");
+System.out.println("it is summer...);
+System.out.println("it is mansoon...");
 }
 }
